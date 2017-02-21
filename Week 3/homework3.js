56a12b82c9f96ad8c233f89ba700998b198282c9
aaleman11 <aaleman11@gmail.com>
2017-02-21 08:04:41 -0800

    Complete HW 3

--- Week 3/homework3.js
+++ Week 3/homework3.js
@@ -1,28 +1,10 @@
-// // // TIMERS
-// console.log('Before')
-// setTimeout(() => {
-//     console.log('inside the timeout')
-// }, 10) // in ms
-// // 1000 ms = 1 sec
-// console.log('After')
-
-// console.log('Before')
-// let count = 0
-// const id = setInterval(() => {
-//     if (count === 5)
-//         clearInterval(id)
-//     else
-//         console.log('repeat')
-
-//     count++
-// }, 10)
-// console.log('After')
-
 //Question 1
 console.log("Question 1")
 
 let count = 5
-const id = setInterval(() => {
+function countDown(count) {
+
+    const id = setInterval(() => {
     if(count==0) {
         clearInterval(id)
     }
@@ -32,9 +14,80 @@ const id = setInterval(() => {
     count--
 
 }, 500)
+    
+}
 
+countDown(5)
 console.log()
 
-//Question 2
+// Question 2
+console.log("Question 2")
 
+let character = 'a'
+
+const isLowerCase = (char) => {
+
+    return new Promise((resolve, reject) => {
+
+        if(char == char.toLowerCase()) {
+            resolve(`this letter was resolved: ${char}`)
+        }
+        else {
+            reject(`this letter was rejected: ${char}`)
+        }
+    })
+}
+
+
+const letters = ['A', 'b', 'c', 'D', 'e']
+
+const sortLetters = (letters) => {
+    const promises = letters.map((char) => {
+        return isLowerCase(char).catch((err) => {
+            return err
+
+        })
+    })
+
+    Promise.all(promises)
+        .then((results) => {
+            console.log(results)
+        })
+}
+
+sortLetters(letters)
+console.log()
+
+//Question 3
+console.log("Question 3")
+
+const compare = (word, wordReversed, callback) => {
+
+    callback (word === wordReversed)
+
+} 
+
+const reverseWord = (word, callback) => {
+
+    let reverse = word.split("").reverse().join("")
+
+    compare(word, reverse, (result) => {
+        
+        callback(result)
+
+    })
+
+}
+
+const print = (word) => {
+
+    reverseWord(word, (result) => {
+        
+        console.log(result)
+
+    })
+}
+
+print('kayak')
+print('canoe')
 
